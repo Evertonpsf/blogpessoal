72c69be32a544d099b638dfea2d9aa790faefbfa
Everton Sales <pinheiroe49@gmail.com>
2024-08-21 12:11:28 -0300

    Componete Home - CSS

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Home from "./pages/home/Home"
+
+function App() {
+  
+  // codigo TypeScript
+
+  return (
+    // codigo TSX => HTML + CSS
+  <>
+      {/*< h1>Truma JavaScript 05 é top</h1>
+    <h2>O React é divertido!</h2>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, numquam dolorem voluptatem hic, sunt et, ipsa corrupti distinctio vero doloribus culpa maiores iste. Voluptate dolorum rem pariatur odit excepturi. Vero. </p >*/}
+<Home/>
+    </>
+  )
+}
+
+export default App
