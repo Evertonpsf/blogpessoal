f87ec6ac582d242a288ee4ca72ce1e3db72a618b
Everton Sales <pinheiroe49@gmail.com>
2024-08-29 14:49:42 -0300

    Crud final blog pessoal - react

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,39 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Footer from "./componets/footer/Footer"
-import Navbar from "./componets/navbar/Navbar"
-import Home from "./pages/home/Home"
-import Cadastro from "./pages/cadastro/Cadastro"
-import Login from "./login/Login"
-import { AuthProvider } from "./contexts/AuthContex"
+import "./App.css";
+
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Home from "./pages/home/Home";
+import { AuthProvider } from "./contexts/AuthContex";
+import Navbar from "./componets/navbar/Navbar";
+import Login from "./pages/login/Login";
+import ListaTemas from "./componets/temas/listaTemas/ListaTemas";
+import FormularioTema from "./componets/temas/formularioTema/FormularioTema";
+import DeletarTema from "./componets/temas/deletarTema/DeletarTema";
+import Footer from "./componets/footer/Footer";
 
 function App() {
-
-  // codigo TypeScript
-
   return (
-    // codigo TSX => HTML + CSS
     <>
-      {/*< h1>Truma JavaScript 05 é top</h1>
-    <h2>O React é divertido!</h2>
-    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, numquam dolorem voluptatem hic, sunt et, ipsa corrupti distinctio vero doloribus culpa maiores iste. Voluptate dolorum rem pariatur odit excepturi. Vero. </p >*/}
       <AuthProvider>
-      <BrowserRouter>
-        <Navbar />
-        <div className="min-h-[80vh]">
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/cadastro" element={<Cadastro />} />
-            <Route path="/home" element={<Home />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
       </AuthProvider>
-      
     </>
-  )
+  );
 }
-
-export default App
+export default App;
